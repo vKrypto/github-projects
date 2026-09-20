c17a1d64199f26302be8859e25321ed90d6a45d2
Ashutosh verma <vkrypto@gmail.com>
2026-09-20 14:45:32 +0530

    Auto Syncing: Auto commit

--- test/revise copy.py
+++ test/revise copy.py
@@ -1,6 +1,17 @@
-l1 = [1,2,12, 11, 21,22,2, 32]
+# l1 = [1,2,12, 11, 21,22,2, 32]
 
-print(l1[0:3])  # [1,2,12]
+# print(l1[0:3])  # [1,2,12]
+
+# print(l1[3:6])
 
 
-print(l1[3:6])
+"""
+
+nums = [10, 20, 30, 40, 50, 60]
+
+Split the list into two lists: first 3 items and last 3 items.
+
+"""
+
+nums = [10, 20, 30, 40, 50, 60]
+
