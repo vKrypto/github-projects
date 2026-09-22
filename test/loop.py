a106a17b5534a010b1ff214efaf2a56875823d83
Ashutosh verma <vkrypto@gmail.com>
2026-09-22 23:30:02 +0530

    Auto Syncing: Auto commit

--- test/loop.py
+++ test/loop.py
@@ -8,11 +8,35 @@ print(x)
 x = [2,3,4,4]
 
 """
-multi_value_varaible = []
-multi_value_varaible = {"k1": 1, ""}
-multi_value_varaible = set((1,23,23,23, "aeqw", "132", 0, 1))
-multi_value_varaible = (3,4,5,56)
+multi_value_varaible = [1, 2, 22, 23,121]
+multi_value_varaible = {"k1": 100000, "k2": 2, 1: 3}
+multi_value_varaible = set((1,23,23,23, "aeqw", "132", 0, 1, -12))
+multi_value_varaible = (3,4,5,56, "daas", [1,23,34])
 multi_value_varaible = "asda"
+multi_value_varaible = [[1,23,34], [2,213,314], [3,123,34]]
 
-for i in multi_value_varaible:
+
+# for i in multi_value_varaible:
+#     # print(i, type(i),  multi_value_varaible[i])
+#     print(i, type(i))
+
+# for ls in multi_value_varaible:
+#     print("====")
+#     for i in ls:
+#         print(i)
+
+# 1- 10
+ls = [1, 2,3,4,5]
+for i in ls:
     print(i)
+
+"""
+range(a, b)
+range(b)
+
+
+a=0 ==> from (included)
+b ==> to (excluded)
+
+
+"""
