1e2cb37903fba01465a632b576fa4ac0dad791fc
Ashutosh verma <vkrypto@gmail.com>
2026-09-22 23:15:02 +0530

    Auto Syncing: Auto commit

---
+++ test/loop.py
@@ -0,0 +1,18 @@
+"""
+list, tuple, dictionory
+
+
+x = 4
+print(x)
+
+x = [2,3,4,4]
+
+"""
+multi_value_varaible = []
+multi_value_varaible = {}
+multi_value_varaible = set()
+multi_value_varaible = ()
+multi_value_varaible = "asda"
+
+for i in multi_value_varaible:
+    print(i)
