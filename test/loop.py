cdab716842e621c7b3c10df78a5d719ebf09ff6b
Ashutosh verma <vkrypto@gmail.com>
2026-09-22 23:15:34 +0530

    Auto Syncing: Auto commit

--- test/loop.py
+++ test/loop.py
@@ -9,9 +9,9 @@ x = [2,3,4,4]
 
 """
 multi_value_varaible = []
-multi_value_varaible = {}
-multi_value_varaible = set()
-multi_value_varaible = ()
+multi_value_varaible = {"k1": 1, ""}
+multi_value_varaible = set((1,23,23,23, "aeqw", "132", 0, 1))
+multi_value_varaible = (3,4,5,56)
 multi_value_varaible = "asda"
 
 for i in multi_value_varaible:
