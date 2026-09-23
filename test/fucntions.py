f48c398131dbd3d33909727ee86b3f69e9c76eab
Ashutosh verma <vkrypto@gmail.com>
2026-09-24 00:00:03 +0530

    Auto Syncing: Auto commit

--- test/fucntions.py
+++ test/fucntions.py
@@ -1,7 +1,43 @@
 """
 functions:
 
-re-usabiltiy: 
+reuse: 
 clean code
 
+
+def p():
+    body
+
+p()
+
+def p(ajit):
+    body
+
+p(rehan)
+    
+if (i < 10):
+    body
+
 """
+
+def print_1_to_10():
+    for i in range(10):
+        print(i)
+
+
+# print_1_to_10()
+
+# print_1_to_10()
+
+def monika():
+    print("monika velly... ghoom rhi")
+    print("monika laddoo... kha rhi")
+
+monika()
+print("---"*20)
+monika()
+
+
+
+def monika(cur_bf):
+    print("")
